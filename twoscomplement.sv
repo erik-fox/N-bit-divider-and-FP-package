dab2d1fa0158432b0e5ae6e8d02b188c83813d55
erik-fox <erikfox589@gmail.com>
2021-02-13 21:39:28 -0800

    Update twoscomplement.sv

--- twoscomplement.sv
+++ twoscomplement.sv
@@ -1,4 +1,4 @@
-module twoscomplement #(parameter N =8)(input [N-1:0]in,output [N-1:0]out);
+module twoscomplement #(parameter N =8)(input bit [N-1:0]in,output bit [N-1:0]out);
 
 assign out = (~(in))+1;
 
