d8deb5f38f78fc0ebc58a1a4849ccc5ce1dbb1d3
erik-fox <erikfox589@gmail.com>
2021-02-13 21:38:48 -0800

    Update nbitshifter.sv

--- nbitshifter.sv
+++ nbitshifter.sv
@@ -1,4 +1,4 @@
-module nbitshifter #(parameter BITSHIFT = 5, parameter BITIN =3 , parameter BITOUT=8) (input [BITIN-1:0]in, output [BITOUT-1:0]out);
+module nbitshifter #(parameter BITSHIFT = 15, parameter BITIN =8 , parameter BITOUT=23) (input bit [BITIN-1:0]in, output bit [BITOUT-1:0]out);
 
 assign out = in<<BITSHIFT;
 
