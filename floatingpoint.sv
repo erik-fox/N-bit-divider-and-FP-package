f7049791d581f04d07aed42e4667fda334442873
erik-fox <erikfox589@gmail.com>
2021-02-15 11:30:46 -0800

    Update floatingpoint.sv

--- floatingpoint.sv
+++ floatingpoint.sv
@@ -1,21 +1,31 @@
 module top();
 import floatingpoint::*;
-shortreal short=2.3, zero= 0.0;
-float fp, nan;
+shortreal short=2.3, zero= 0.0, test;
+float fp, nan, inf, fptest;
+
   
 initial
   begin
     nan.sign='0;
     nan.exponent='1;
     nan.fraction='1;
+   
+    inf.sign=0;
+    inf.exponent='1;
+    inf.fraction='0;
+    
     #10
     $dumpfile("dump.vcd"); $dumpvars;
 	fp=fpnumberfromshortreal(short);
     printfp(fp);
     if(isnan(nan))
-      $display("NaN");
+        $display("NaN");
     if(iszero(fpnumberfromshortreal(zero)))
         $display("ZERO");
-
+    if(isinfinity(inf))
+        $display("infinity");
+    test=shortrealfromfpnumber(fp);
+    fptest=fpnumberfromcomponents(fp.sign,fp.exponent,fp.fraction);
+    printfp(fptest);
   end
 endmodule
