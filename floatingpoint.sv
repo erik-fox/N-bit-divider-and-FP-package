09e1b6724b4b4c2813b8bae9daafe9db8de0e67a
Erik Fox <erfox@auto.ece.pdx.edu>
2021-02-15 15:53:56 -0800

    ready to test

--- floatingpoint.sv
+++ floatingpoint.sv
@@ -5,27 +5,36 @@ float fp, nan, inf, fptest;
 
   
 initial
-  begin
-    nan.sign='0;
-    nan.exponent='1;
-    nan.fraction='1;
+begin
+    	$dumpfile("dump.vcd"); $dumpvars;
+	
+	nan.sign='0;
+    	nan.exponent='1;
+    	nan.fraction='1;
    
-    inf.sign=0;
-    inf.exponent='1;
-    inf.fraction='0;
+ 	inf.sign=0;
+    	inf.exponent='1;
+    	inf.fraction='0;
     
-    #10
-    $dumpfile("dump.vcd"); $dumpvars;
 	fp=fpnumberfromshortreal(short);
-    printfp(fp);
-    if(isnan(nan))
-        $display("NaN");
-    if(iszero(fpnumberfromshortreal(zero)))
-        $display("ZERO");
-    if(isinfinity(inf))
-        $display("infinity");
-    test=shortrealfromfpnumber(fp);
-    fptest=fpnumberfromcomponents(fp.sign,fp.exponent,fp.fraction);
-    printfp(fptest);
-  end
+	
+    	printfp(fp);
+
+    	if(isnan(nan))
+        	$display("NaN1");
+    	if(isnan(fpnumberfromshortreal(zero)))
+        	$display("NaN2");
+    	if(isnan(inf))
+        	$display("NaN3");
+    	if(isnan(fp))
+        	$display("NaN4");
+
+    	if(iszero(fpnumberfromshortreal(zero)))
+        	$display("ZERO");
+    	if(isinfinity(inf))
+        	$display("infinity");
+    	test=shortrealfromfpnumber(fp);
+    	fptest=fpnumberfromcomponents(fp.sign,fp.exponent,fp.fraction);
+    	printfp(fptest);
+end
 endmodule
