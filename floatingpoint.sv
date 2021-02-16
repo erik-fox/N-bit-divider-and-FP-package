03093eb9558732aaf3bad13ed35a3c5ab29e1ac5
erik-fox <erikfox589@gmail.com>
2021-02-15 16:14:32 -0800

    Update floatingpoint.sv

--- floatingpoint.sv
+++ floatingpoint.sv
@@ -2,7 +2,7 @@ module top();
 import floatingpoint::*;
 shortreal short=2.3, zero= 0.0, test;
 float fp, nan, inf, fptest;
-
+logic nan1,nan2,nan3,nan4;
   
 initial
 begin
@@ -11,16 +11,19 @@ begin
 	nan.sign='0;
     	nan.exponent='1;
     	nan.fraction='1;
-   
- 	inf.sign=0;
+  
+	inf.sign=0;
     	inf.exponent='1;
     	inf.fraction='0;
     
 	fp=fpnumberfromshortreal(short);
 	
     	printfp(fp);
-
-    	if(isnan(nan))
+ 	printfp(fpnumberfromshortreal(zero));
+  	printfp(inf);
+  	printfp(nan);
+    	
+  	if(isnan(nan))
         	$display("NaN1");
     	if(isnan(fpnumberfromshortreal(zero)))
         	$display("NaN2");
@@ -30,10 +33,26 @@ begin
         	$display("NaN4");
 
     	if(iszero(fpnumberfromshortreal(zero)))
-        	$display("ZERO");
-    	if(isinfinity(inf))
-        	$display("infinity");
+          	$display("ZERO1");
+ 	if(iszero(nan))
+    		$display("ZERO2");    	
+  	if(iszero(fp))
+          	$display("ZERO3");    				
+  	if(iszero(inf))
+            $display("ZERO4");
+  
+  	if(isinfinity(inf))
+         	$display("infinity1");
+  	if(isinfinity(nan))
+          	$display("infinity2");
+  	if(isinfinity(fp))
+          	$display("infinity3");
+   	if(isinfinity(fpnumberfromshortreal(zero)))
+         	 $display("infinity4");
+  
     	test=shortrealfromfpnumber(fp);
+  	$display("Short real: %f",test);
+  
     	fptest=fpnumberfromcomponents(fp.sign,fp.exponent,fp.fraction);
     	printfp(fptest);
 end
