f6ec1672bac8a730b8a7ec426493649831ee088a
erik-fox <erikfox589@gmail.com>
2021-02-15 16:50:27 -0800

    Update floatingpoint.sv

--- floatingpoint.sv
+++ floatingpoint.sv
@@ -1,12 +1,16 @@
 module top();
 import floatingpoint::*;
 shortreal short=2.3, zero= 0.0, test;
-float fp, nan, inf, fptest;
-logic nan1,nan2,nan3,nan4;
+float fp, nan, inf, fptest, denorm;
+
   
 initial
 begin
     	$dumpfile("dump.vcd"); $dumpvars;
+	
+	denorm.sign='0;
+	denorm.exponent='0;
+	denorm.fraction='1;
 	
 	nan.sign='0;
     	nan.exponent='1;
@@ -18,11 +22,23 @@ begin
     
 	fp=fpnumberfromshortreal(short);
 	
+	printfp(denorm);
     	printfp(fp);
  	printfp(fpnumberfromshortreal(zero));
   	printfp(inf);
   	printfp(nan);
     	
+	if(isdenorm(denorm))
+		$display("Denorm1");
+	if(isdenorm(inf))
+		$display("Denorm2");
+	if(isdenorm(nan))
+		$display("Denorm3");
+	if(isdenorm(fp))
+		$display("Denorm4");
+	if(isdenorm(fpnumberfromshortreal(zero)))
+		$display("Denorm5");
+	
   	if(isnan(nan))
         	$display("NaN1");
     	if(isnan(fpnumberfromshortreal(zero)))
@@ -31,6 +47,8 @@ begin
         	$display("NaN3");
     	if(isnan(fp))
         	$display("NaN4");
+	if(isnan(denorm))
+		$display("NaN5");
 
     	if(iszero(fpnumberfromshortreal(zero)))
           	$display("ZERO1");
@@ -39,8 +57,10 @@ begin
   	if(iszero(fp))
           	$display("ZERO3");    				
   	if(iszero(inf))
-            $display("ZERO4");
-  
+            	$display("ZERO4");
+	if(iszero(denorm))
+		$display("ZERO5");
+	
   	if(isinfinity(inf))
          	$display("infinity1");
   	if(isinfinity(nan))
@@ -49,6 +69,8 @@ begin
           	$display("infinity3");
    	if(isinfinity(fpnumberfromshortreal(zero)))
          	 $display("infinity4");
+	if(isinfinity(denorm))
+		$display("infinity5");
   
     	test=shortrealfromfpnumber(fp);
   	$display("Short real: %f",test);
